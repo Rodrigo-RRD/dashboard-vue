61cc96a4589697e9658e5e83945586b858ba2a27
rodrigorrd <116466164+Rodrigo-RRD@users.noreply.github.com>
2024-11-22 20:52:31 -0300

    bug: corrigindo text white no about

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import global from './assets/js/global';
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 import { BootstrapVue3 } from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
